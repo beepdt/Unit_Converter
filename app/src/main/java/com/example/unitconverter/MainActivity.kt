e8331c37fce38a002953856f4a4feb63791de8b7
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-07-13 01:23:59 +0530

    Spacer and Drop down menu

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -7,10 +7,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,12 +55,17 @@ fun UnitConverter(){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Unit Converter")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             value = "",
             onValueChange = {},
             placeholder = {
                 Text(text = "Enter Value")
         })
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
             Box {
@@ -64,15 +75,51 @@ fun UnitConverter(){
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
-            }
-            Box {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Select")
-                    Icon(Icons.Default.ArrowDropDown,
-                        contentDescription = "Arrow Down")
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters")},
+                        onClick = { /*TODO*/ })
                 }
             }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Box {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Select")
+                        Icon(Icons.Default.ArrowDropDown,
+                            contentDescription = "Arrow Down"
+                        )
+                    }
+                    DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenuItem(
+                            text = { Text(text = "Centimeters")},
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Meters")},
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Feet")},
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Millimeters")},
+                            onClick = { /*TODO*/ })
+                    }
+                }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Result")
     }
 }
 
